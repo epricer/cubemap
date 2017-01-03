ff76d27902b1f0a9060eab171e1a4c8ea7cac260
Edward Pricer <Edward Pricer>
2017-01-02 21:35:41 -0500

    completed namespacing

--- index.js
+++ index.js
@@ -1,233 +1,256 @@
-var quoteTimer = null;
-var employeeModel;
-var mapModel;
-var quotesModel;
-var cameraStream;
+$(document).ready(function() {
+    mapapp.initEvents();
+    mapapp.init();
+});
 
-function trackCursor(mouseEvent) {
-    var xPosition = mouseEvent.clientX / getWindowWidth();
-    var yPosition = mouseEvent.clientY / getWindowHeight();
+var mapapp = (function () {
 
-    var zoomWidth = getZoomWidth();
-    var zoomHeight = getZoomHeight();
-    var left = (zoomWidth !== null) ? (getWindowWidth() - zoomWidth) * xPosition : 0;
-    var top = (zoomHeight !== null) ? (getWindowHeight() - zoomHeight) * yPosition : 0;
-    $("#map").css("left", left);
-    $("#map").css("top", top);
-}
+    var quoteTimer = null;
+    var employeeModel;
+    var mapModel;
+    var quotesModel;
+    var cameraStream;
 
-function getZoom() {
-    return 1.3;
-}
+    return {
+        initEvents: function() {
+            $("#body").on('resize', mapapp.drawMap);
+            $("#body").on('mousemove', mapapp.trackCursor);
+            $("#deletebutton").on('click', mapapp.deleteEmployee);
+            $("#savebutton").on('click', mapapp.commitModification);
+            $("#closebutton").on('click', mapapp.closeModifyDialog);
+            $("#photo").on('click', mapapp.showCamera);
+            $("#video").on('click', mapapp.takePhoto);
+            $("#mascot").on('click', mapapp.showQuote);
+        },
 
-function getWindowWidth() {
-    return $(window).width();
-}
+        init: function () {
+            
+            $.when(
+                $.ajax({ url: "/employees", cache: false }).done(function (data) {
+                    employeeModel = data;
+                }),
+                $.ajax({ url: "/map", cache: false }).done(function (data) {
+                    mapModel = data;
+                }),
+                $.ajax({ url: "/quotes", cache: false }).done(function (data) {
+                    quotesModel = data;
+                })
+            ).then(function () {
+                mapapp.drawMap();
+            });
+        },
 
-function getWindowHeight() {
-    return $(window).height();
-}
+        trackCursor: function (mouseEvent) {
+            var xPosition = mouseEvent.clientX / mapapp.getWindowWidth();
+            var yPosition = mouseEvent.clientY / mapapp.getWindowHeight();
 
-function getZoomWidth() {
-    if (mapModel.width * getAutoScale(mapModel.width, mapModel.height) * getZoom() < getWindowWidth()) return null;
-    return getWindowWidth() * getZoom();
-}
+            var zoomWidth = mapapp.getZoomWidth();
+            var zoomHeight = mapapp.getZoomHeight();
+            var left = (zoomWidth !== null) ? (mapapp.getWindowWidth() - zoomWidth) * xPosition : 0;
+            var top = (zoomHeight !== null) ? (mapapp.getWindowHeight() - zoomHeight) * yPosition : 0;
+            $("#map").css("left", left);
+            $("#map").css("top", top);
+        },
 
-function getZoomHeight() {
-    if (mapModel.height * getAutoScale(mapModel.width, mapModel.height) * getZoom() < getWindowHeight()) return null;
-    return getWindowHeight() * getZoom();
-}
+        getZoom: function () {
+            return 1.3;
+        },
 
-function drawMap() {
-    $("#map").css("transform", "scale(" + getZoom() + "," + getZoom() + ")");
-    $("#map").empty();
+        getWindowWidth: function () {
+            return $(window).width();
+        },
 
-    for (var i = 0; i < mapModel.structuregroups.length; i++) {
-        var sg = mapModel.structuregroups[i];
-        drawStructures(sg.structures, mapModel.width, mapModel.height, sg.type);
-    }
-}
+        getWindowHeight: function () {
+            return $(window).height();
+        },
 
+        getZoomWidth: function () {
+            if (mapModel.width * mapapp.getAutoScale(mapModel.width, mapModel.height) * mapapp.getZoom() < mapapp.getWindowWidth()) return null;
+            return mapapp.getWindowWidth() * mapapp.getZoom();
+        },
 
-function init() {
+        getZoomHeight: function () {
+            if (mapModel.height * mapapp.getAutoScale(mapModel.width, mapModel.height) * mapapp.getZoom() < mapapp.getWindowHeight()) return null;
+            return mapapp.getWindowHeight() * mapapp.getZoom();
+        },
 
-    $.when(
-        $.ajax({ url: "/employees", cache: false }).done(function(data) {
-            employeeModel = data;
-        }),
-        $.ajax({ url: "/map", cache: false }).done(function(data) {
-            mapModel = data;
-        }),
-        $.ajax({ url: "/quotes", cache: false }).done(function(data) {
-            quotesModel = data;
-        })
-    ).then(function() {
-        drawMap();
-    });
-}
+        drawMap: function () {
+            $("#map").css("transform", "scale(" + mapapp.getZoom() + "," + mapapp.getZoom() + ")");
+            $("#map").empty();
 
+            for (var i = 0; i < mapModel.structuregroups.length; i++) {
+                var sg = mapModel.structuregroups[i];
+                mapapp.drawStructures(sg.structures, mapModel.width, mapModel.height, sg.type);
+            }
+        },
 
-function drawStructures(structures, mapWidth, mapHeight, type) {
-    var scale = getAutoScale(mapWidth, mapHeight);
-    for (var i = 0; i < structures.length; i++) {
-        var structure = structures[i];
-        var structureDiv = $("<div></div>");
-        structureDiv.addClass(type);
-        structureDiv.css("width", structure.width * scale);
-        structureDiv.css("height", structure.height * scale);
-        structureDiv.css("left", (mapWidth - structure.x - structure.width) * scale);
-        structureDiv.css("top", (mapHeight - structure.y - structure.height) * scale);
-        structureDiv.attr("id", structure.name); // todo: is this needed?
-        structureDiv.data("structure", structure);
-
-        var employee = getEmployeeByStructure(structure.name);
-        if (employee !== null) {
-            structureDiv.html(employee.name.split(" ").join("<br />"));
-        } else {
-            structureDiv.html(structure.name);
-        }
-
-        if (structure.editable) {
-            structureDiv.click(showModifyDialog.bind(this, structure));
-            structureDiv.css('cursor', 'pointer');
-        }
-
-        $("#map").append(structureDiv);
-    }
-}
-
-function getAutoScale(mapWidth, mapHeight) {
-    // calculate a scaling factor that maximizes the map in the available screen
-    var scaleX = $(window).width() / mapWidth;
-    var scaleY = $(window).height() / mapHeight;
-    return Math.floor(Math.min(scaleX, scaleY));
-}
-
-function getQuote() {
-    var i = Math.round(Math.random() * (quotesModel.length - 1));
-    return quotesModel[i].quote;
-}
-
-function getEmployeeByStructure(structureName) {
-
-    for (var i = 0; i < employeeModel.length; i++) {
-        if (employeeModel[i].structure === structureName) return employeeModel[i];
-    }
-    return null;
-}
-
-function showQuote() {
-    hideQuote();
-    $("#bubble").css("display", "block");
-    $("#quote").css("display", "block");
-    $("#quote").html(getQuote());
-    quoteTimer = window.setTimeout(hideQuote, 3000);
-}
-
-function hideQuote() {
-    window.clearTimeout(quoteTimer);
-    $("#bubble").css("display", "none");
-    $("#quote").css("display", "none");
-}
-
-function showModifyDialog(structure) {
-    var employee = getEmployeeByStructure(structure.name);
-    var employeeName = (employee === null) ? "" : employee.name;
-    $("#map").css("filter", "blur(3px)");
-
-    $("#location").val(structure.name);
-    $("#name").val(employeeName);
-
-    $("#photo").attr("src", (employee && employee.photo) ? employee.photo : "missing.png");
-    $("#name").data("oldvalue", employeeName);
-    $("#modification").css("display", "block");
-    $("#name").focus();
-}
-
-function closeModifyDialog() {
-    $("#map").css("filter", "");
-    $("#modification").css("display", "none");
-    $("#controls").css("display", "block");
-    $("#camera").css("display", "none");
-    hideCamera();
-}
-
-function hideCamera() {
-    var tracks = cameraStream.getTracks();
-    for (var i = 0; i < tracks.length; i++) {
-        tracks[i].stop();
-        $("#video").removeAttr("src");
-    }
-    $("#video").attr("src", null);
-}
+        drawStructures: function (structures, mapWidth, mapHeight, type) {
+            var scale = mapapp.getAutoScale(mapWidth, mapHeight);
+            for (var i = 0; i < structures.length; i++) {
+                var structure = structures[i];
+                var structureDiv = $("<div></div>");
+                structureDiv.addClass(type);
+                structureDiv.css("width", structure.width * scale);
+                structureDiv.css("height", structure.height * scale);
+                structureDiv.css("left", (mapWidth - structure.x - structure.width) * scale);
+                structureDiv.css("top", (mapHeight - structure.y - structure.height) * scale);
+                structureDiv.attr("id", structure.name); // todo: is this needed?
+                structureDiv.data("structure", structure);
 
 
 
-function showCamera() {
+                var employee = mapapp.getEmployeeByStructure(structure.name);
+                if (employee !== null) {
+                    structureDiv.html(employee.name.split(" ").join("<br />"));
+/*                    var photoDiv = $("<img></img>");
+                    photoDiv.attr("src", employee.photo);
+                    photoDiv.addClass("mapphoto");
+                    structureDiv.append(photoDiv);*/
+                } else {
+                    structureDiv.html(structure.name);
+                }
 
-    var mediaConfig = { video: true };
+                if (structure.editable) {
+                    structureDiv.click(mapapp.showModifyDialog.bind(this, structure));
+                    structureDiv.css('cursor', 'pointer');
+                }
 
-    // Put video listeners into place
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
-            cameraStream = stream;
+                $("#map").append(structureDiv);
+            }
+        },
+
+        getAutoScale: function (mapWidth, mapHeight) {
+            // calculate a scaling factor that maximizes the map in the available screen
+            var scaleX = $(window).width() / mapWidth;
+            var scaleY = $(window).height() / mapHeight;
+            return Math.floor(Math.min(scaleX, scaleY));
+        },
+
+        getQuote: function () {
+            var i = Math.round(Math.random() * (quotesModel.length - 1));
+            return quotesModel[i].quote;
+        },
+
+        getEmployeeByStructure: function (structureName) {
+
+            for (var i = 0; i < employeeModel.length; i++) {
+                if (employeeModel[i].structure === structureName) return employeeModel[i];
+            }
+            return null;
+        },
+
+        showQuote: function () {
+            mapapp.hideQuote();
+            $("#bubble").css("display", "block");
+            $("#quote").css("display", "block");
+            $("#quote").html(getQuote());
+            quoteTimer = window.setTimeout(hideQuote, 3000);
+        },
+
+        hideQuote: function () {
+            window.clearTimeout(quoteTimer);
+            $("#bubble").css("display", "none");
+            $("#quote").css("display", "none");
+        },
+
+        showModifyDialog: function (structure) {
+            var employee = mapapp.getEmployeeByStructure(structure.name);
+            var employeeName = (employee === null) ? "" : employee.name;
+            $("#map").css("filter", "blur(3px)");
+
+            $("#location").val(structure.name);
+            $("#name").val(employeeName);
+
+            $("#photo").attr("src", (employee && employee.photo) ? employee.photo : "missing.png");
+            $("#name").data("oldvalue", employeeName);
+            $("#modification").css("display", "block");
+            $("#name").focus();
+        },
+
+        closeModifyDialog: function () {
+            $("#map").css("filter", "");
+            $("#modification").css("display", "none");
+            $("#controls").css("display", "block");
+            $("#camera").css("display", "none");
+            mapapp.hideCamera();
+        },
+
+        hideCamera: function () {
+            if (cameraStream) {
+                var tracks = cameraStream.getTracks();
+                for (var i = 0; i < tracks.length; i++) {
+                    tracks[i].stop();
+                    $("#video").removeAttr("src");
+                }
+            }
+            $("#video").attr("src", null);
+        },
+
+        showCamera: function () {
+            var mediaConfig = { video: true };
+
+            // Put video listeners into place
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia(mediaConfig).then(function (stream) {
+                    cameraStream = stream;
+                    var video = $('#video')[0];
+                    $("#controls").css("display", "none");
+                    $("#camera").css("display", "block");
+                    video.src = window.URL.createObjectURL(cameraStream);
+                    video.play();
+
+                }).catch(function (err) {
+                    alert("Can't access camera");
+                });
+            }
+        },
+
+        takePhoto: function () {
+            var still = $("#still")[0];
+            var stillContext = still.getContext('2d');
+
+            var crop = $("#crop")[0];
+            var cropContext = crop.getContext('2d');
+
             var video = $('#video')[0];
-            $("#controls").css("display", "none");
-            $("#camera").css("display", "block");
-            video.src = window.URL.createObjectURL(cameraStream);
-            video.play();
 
-        }).catch(function(err) {
-            alert("Can't access camera");
-        });
-    }
-}
+            stillContext.drawImage(video, 0, 0, 120, 90);
+            cropContext.drawImage(still, 20, 0, 80, 90, 0, 0, 80, 90);
+            $("#photo").attr("src", crop.toDataURL("image/jpeg", 0.7));
+            $("#controls").css("display", "block");
+            $("#camera").css("display", "none");
+            mapapp.hideCamera();
+        },
 
-function takePhoto() {
-    var still = $("#still")[0];
-    var stillContext = still.getContext('2d');
+        deleteEmployee: function () {
+            var name = $("#name").val().trim();
 
-    var crop = $("#crop")[0];
-    var cropContext = crop.getContext('2d');
+            if (name === "")
+                alert("You must select a name to delete");
+            else {
+                $.post("/delete?name=" + name)
+                    .done(function () {
+                        mapapp.closeModifyDialog();
+                        mapapp.init();
+                    });
+            }
+        },
 
-    var video = $('#video')[0];
+        commitModification: function () {
 
-    stillContext.drawImage(video, 0, 0, 120, 90);
-    cropContext.drawImage(still, 20, 0, 80, 90, 0, 0, 80, 90);
-    $("#photo").attr("src", crop.toDataURL("image/jpeg", 0.7));
-    $("#controls").css("display", "block");
-    $("#camera").css("display", "none");
-    hideCamera();
-}
+            var employeeName = $("#name").val().trim();
+            var newLocation = $("#location").val().trim();
+            var photo = $("#photo").attr("src");
+            if ((employeeName === "") || (newLocation === "")) {
+                alert("You must enter a name (Firstname Lastname) and provide a location (cube number)");
+            } else {
+                $.post("/update", JSON.stringify({ "name": employeeName, "structure": newLocation, "previousname": $("#name").data("oldvalue"), "photo": photo }))
+                    .done(function (data) {
+                        mapapp.closeModifyDialog();
+                        mapapp.init();
+                    });
+            }
+        }
+    };
+})();
 
-
-
-function deleteEmployee() {
-    var name = $("#name").val().trim();
-
-    if (name === "")
-        alert("You must select a name to delete");
-    else {
-        $.post("/delete?name=" + name)
-            .done(function() {
-                closeModifyDialog();
-                init();
-            });
-    }
-}
-
-function commitModification() {
-
-    var employeeName = $("#name").val().trim();
-    var newLocation = $("#location").val().trim();
-    var photo = $("#photo").attr("src");
-    if ((employeeName === "") || (newLocation === "")) {
-        alert("You must enter a name (Firstname Lastname) and provide a location (cube number)");
-    } else {
-        $.post("/update", JSON.stringify({ "name": employeeName, "structure": newLocation, "previousname": $("#name").data("oldvalue"), "photo": photo }))
-            .done(function(data) {
-                closeModifyDialog();
-                init();
-            });
-    }
-}
